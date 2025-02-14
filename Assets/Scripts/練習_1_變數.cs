248192b32f8db9e73c4b29d1e543b6c50c9c96db
lucky86622 <lucky86622@gmail.com>
2025-02-14 19:34:36 +0800

    Update 練習

---
+++ Assets/Scripts/練習_1_變數.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+
+public class NewEmptyCSharpScript : MonoBehaviour
+{
+    public short prise = 999;
+    private float jumpHigh = 77.5f;
+    public string weapon = "三項之力";
+    private bool dragonSoul = true;
+
+}
---
+++ Assets/Scripts/練習_2_資料類型.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class 練習_2_資料類型 : MonoBehaviour
+{
+    public float high = 7.7f;
+    public double dog = 0.12345d;
+    public decimal cat = 1.23456789m;
+}
