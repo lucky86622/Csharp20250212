6ff07e150777faca97814043daee86816504c6fc
lucky86622 <lucky86622@gmail.com>
2025-02-19 17:49:29 +0800

    Update 事件與有錢文字

--- Assets/Scripts/Class_3_Operator.cs
+++ Assets/Scripts/Class_3_Operator.cs
@@ -23,10 +23,9 @@ public class Class_3_Operator : MonoBehaviour
     // [文字範圍(最小行,最大行)]：使用在字串上，設定面板上的文字框範圍
     [TextArea(2, 5)]
     public string itemDescription = "這裡是道具的描述，這裡是道具的描述，這裡是道具的描述。";
-    #endregion
-
     // 多個欄位屬性
-    [Header("血量")][Range(0, 999)]
+    [Header("血量")]
+    [Range(0, 999)]
     public int hp = 100;
     [Header("攻擊力"), Range(0, 100)]
     public float attack = 30.5f;
@@ -36,4 +35,28 @@ public class Class_3_Operator : MonoBehaviour
     // [序列化欄位]：將私人變數顯示
     [SerializeField]
     private float mp = 500;
+    #endregion
+
+    // 使用 Unity 的事件
+    // 1. 必須在腳本後面添加：MonoBehaviour (繼承)
+    // 2. 使用關鍵字快速完成，例如：輸入 Awake 挑選要使用的事件按 Ente
+    // 喚醒事件：撥放遊戲後會執行的第一個事件，只執行一次
+    // 事件名稱是藍色的 (沒有變色不影響)
+    private void Awake()
+    {
+        // 將小括號內的訊息輸出到 Unity 的 Console 控制台面板
+        // Ctrl + Shift + C 開啟控制台面板
+        Debug.Log("哈囉，沃德 :D");     //輸出文字
+        Debug.Log(hp);                 //輸出變數的值
+        Debug.Log("攻擊力：" + attack); //字串與變數
+        Debug.Log($"攻擊力：{attack}"); //$字串格式
+        Debug.Log("攻擊力：{attack}");  //沒有寫$
+
+        Debug.Log("<b>粗體</b>");
+        Debug.Log("<color=orange>橘色</color>");
+        // 色碼： #紅紅綠綠藍藍，數值：0 ~ 9，a ~ f 越右邊顏色越亮 #000 黑 #fff 白
+        Debug.Log("<color=#66aaff>藍色</color>");
+        Debug.Log("<color=#6af>藍色</color>");
+
+    }
 }
