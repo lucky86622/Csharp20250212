10aa9d21ce6922c715074853905952bf8450f7e0
lucky86622 <lucky86622@gmail.com>
2025-03-08 13:00:26 +0800

    Update 迴圈

--- Assets/Scripts/Class_5_2_enumeration.cs
+++ Assets/Scripts/Class_5_2_enumeration.cs
@@ -55,6 +55,28 @@ namespace BAG
         private void Start()
         {
             Debug.Log((int)item);
+
+            // 列舉判斷式 switch
+            // switch + Tab * 2 > 修改 switch_on 為列舉 > Enter * 2 快速完成
+            switch (item)
+            {
+                case Item.None:
+                    Debug.Log("沒有道具");
+                    break;
+                case Item.Coin:
+                    Debug.Log("金幣");
+                    break;
+                case Item.RedWater:
+                case Item.BlueWater:
+                    Debug.Log("藥水道具");
+                    break;
+                case Item.Chicken:
+                    Debug.Log("炸雞");
+                    break;
+                default:
+                    Debug.Log("沒有道具");
+                    break;
+            }
         }
     }
 
