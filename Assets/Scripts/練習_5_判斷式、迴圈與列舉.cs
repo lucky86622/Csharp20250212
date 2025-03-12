0b0a7f25452eab623f4271d4bec5d08b5597e5b6
lucky86622 <lucky86622@gmail.com>
2025-03-12 16:10:35 +0800

    Update 作業

--- Assets/Scripts/練習_5_判斷式、迴圈與列舉.cs
+++ Assets/Scripts/練習_5_判斷式、迴圈與列舉.cs
@@ -3,13 +3,26 @@ using UnityEngine;
 public class 練習_5_判斷式 : MonoBehaviour
 {
     [SerializeField, Header("血量"), Range(0, 100)]   
-    private int Hp = 100;
+    private float Hp = 100;
 
     private void Update()
     {
         switch (Hp)
         {
-            default:
+            case >= 80:
+                Debug.Log("<color=#68f>血量安全</color>");
+                break;
+            case >= 60:
+                Debug.Log("<color=#3f3>健康狀態有狀況</color>");
+                break;
+            case >= 40:
+                Debug.Log("<color=#f39>警告，快喝水</color>");
+                break;
+            case >= 10:
+                Debug.Log("<color=#f33>快死掉了</color>");
+                break;
+            case <= 0:
+                Debug.Log("<color=#f11>已經死了</color>");
                 break;
         }
     }
