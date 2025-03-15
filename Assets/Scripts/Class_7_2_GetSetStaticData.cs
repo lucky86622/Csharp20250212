fdf1b2f9ce3938bb22c4e6b1739b6178a98ae857
lucky86622 <lucky86622@gmail.com>
2025-03-15 13:26:31 +0800

    Update 靜態與非靜態

--- Assets/Scripts/Class_7_2_GetSetStaticData.cs
+++ Assets/Scripts/Class_7_2_GetSetStaticData.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// 練習取得與設定靜態資料
+/// </summary>
 public class Class_7_2_GetSetStaticData : MonoBehaviour
 {
     // 取得非靜態資料 (成員)
@@ -24,5 +27,22 @@ public class Class_7_2_GetSetStaticData : MonoBehaviour
         Debug.Log($"<color=#f3d>靜態屬性：{Class_7_2_Static.skillSecond}</color>");
         // 呼叫靜態方法
         Class_7_2_Static.kick();
+    }
+
+    public Class_7_2_Slime slimeGreen, slimeBlue;
+
+    private void Start()
+    {
+        // 非靜態成員
+        slimeGreen.hp -= 10;
+        Debug.Log($"<color=#3f3>綠史萊姆 HP：{slimeGreen.hp}</color>");
+        slimeBlue.hp -= 10;
+        Debug.Log($"<color=#3f3>藍史萊姆 HP：{slimeBlue.hp}</color>");
+
+        // 靜態成員
+        // 靜態成員沒辦法透過個體存取
+        // slimeGreen.moveSpeed = 3.5f;
+        Class_7_2_Slime.moveSpeed = 3.5f;
+        Debug.Log($"<color=#3f3>史萊姆移動速度：{Class_7_2_Slime.moveSpeed}</color>");
     }
 }
--- Assets/Scripts/Class_7_2_Static.cs
+++ Assets/Scripts/Class_7_2_Static.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class Class_7_2_Static : MonoBehaviour
 {
+    #region 變數與屬性
     // 成員：
     // 變數、屬性、方法
 
@@ -19,6 +20,10 @@ public class Class_7_2_Static : MonoBehaviour
     public string skillMain => "火球術";
     // 靜態屬性
     public static string skillSecond => "治癒術";
+    #endregion
+
+    private float attack = 10;
+    private static float mp = 100;
 
     private void Awake()
     {
@@ -31,11 +36,18 @@ public class Class_7_2_Static : MonoBehaviour
     public void Punch()
     {
         Debug.Log("<color=#3f3>使用拳擊</color>");
+        // 非靜態方法內可以存取所有成員
+        Debug.Log($"<color=#f9e>非靜態攻擊力：{attack}</color>");
+        Debug.Log($"<color=#f9e>靜態魔力：{mp}</color>");
     }
 
     public static void kick()
     {
         Debug.Log("<color=#3f3>使用踢擊</color>");
+        // 靜態方法內只能存取靜態成員
+        // 由於 attack 是非靜態所以無法存取 (導致錯誤)
+        // Debug.Log($"<color=#f9e>非靜態攻擊力：{attack}</color>");
+        Debug.Log($"<color=#f9e>靜態魔力：{mp}</color>");
     }
 
     private void Start()
