905956d06f91f63fab4f8143efd3a624ae739fc5
lucky86622 <lucky86622@gmail.com>
2025-03-16 13:24:41 +0800

    Update 類別

---
+++ Assets/Scripts/Class_8_1_Class.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 類別 Class
+/// </summary>
+public class Class_8_1_Class : MonoBehaviour
+{
+    // 類別：
+    // 用來定義一個物件以及他所擁有的資料與功能 (成員)
+
+    private void Awake()
+    {
+        Class_8_1_NPC npcJack = new Class_8_1_NPC();
+        Class_8_1_NPC npcKID = new Class_8_1_NPC("KID");
+
+        npcJack.Logname();
+        npcKID.Logname();
+    }
+}
