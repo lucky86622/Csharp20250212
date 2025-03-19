3c2738f090a607b80c936ba9b1885f220180e400
lucky86622 <lucky86622@gmail.com>
2025-03-19 17:14:44 +0800

    Update 作業

---
+++ Assets/Scripts/練習_7_屬性與靜態.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class 練習_7_屬性與靜態 : MonoBehaviour
+{
+    [SerializeField]
+    private float _hp = 100;
+
+    public float hp
+    {
+        get
+        {
+            if (_hp <= 0) Debug.Log("<color=#f33>玩家角色已死亡</color>");
+            return _hp;
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(hp);
+    }
+}
---
+++ Assets/Scripts/練習_8_類別.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class 練習_8_類別 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Practice_8_Boss bossDragon = new Practice_8_Boss("烈咬陸鯊", "地震", 239);
+        Practice_8_Boss bossGhost = new Practice_8_Boss("雪妖女", "暴風雪", 138);
+
+        Debug.Log($"<color=#f93>{bossDragon.name}, 招式：{bossDragon.skill}</color>");
+        Debug.Log($"<color=#f93>{bossGhost.name}, 招式：{bossGhost.skill}</color>");
+    }
+}
+
+public class Practice_8_Boss
+{
+    public string name;
+    public string skill;
+    public float hp;
+
+    public Practice_8_Boss(string _name, string _skill, float _hp)
+    {
+        name = _name;
+        skill = _skill;
+        hp = _hp;
+    }
+}
