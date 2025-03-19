e38fc9de2e8744aee54437f68ff52315b2c0221e
lucky86622 <lucky86622@gmail.com>
2025-03-19 20:16:30 +0800

    Update 資料類型的轉換

---
+++ Assets/Scripts/Class_8_4_Class.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Class_8_4_Class
+{
+    public string name;
+
+    public Class_8_4_Class(string _name)
+    {
+        name = _name;
+    }
+}
---
+++ Assets/Scripts/Class_8_4_Struct.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public struct Class_8_4_Struct
+{
+    public string name;
+
+    public Class_8_4_Struct(string _name)
+    {
+        name = _name;
+    }
+}
