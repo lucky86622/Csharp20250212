f8f07c254bb09fb13ab87a6cb874cfd2d6e55b05
lucky86622 <lucky86622@gmail.com>
2025-04-17 13:46:03 +0800

    Update LinQ

--- Assets/Scripts/Class_19_LinQ.cs
+++ Assets/Scripts/Class_19_LinQ.cs
@@ -9,9 +9,12 @@ using System.Linq;   // 引用 LinQ 命名空間
 public class Class_19_LinQ : MonoBehaviour
 {
     public int[] scores = { 90, 80, 77, 88, 93, 1, 5, 10, 35 };
+    public string[] items = { "紅色藥水", "藍色藥水", "地圖", "回家卷軸", "匕首", "紅色炸彈" };
+    public int[] numbers = { 1, 23, 77, 34, 42, 56, 80 };
 
     private void Awake()
     {
+        #region LinQ 基本用法
         // 使用 LinQ 的關鍵字查詢資料
         // 起手式：
         // from 資料來源
@@ -32,7 +35,7 @@ public class Class_19_LinQ : MonoBehaviour
                          orderby score descending
                          select score;
 
-        foreach(var item in querySixty)
+        foreach (var item in querySixty)
         {
             LogSystem.LogWithColor(item, "#ff3");
         }
@@ -42,16 +45,57 @@ public class Class_19_LinQ : MonoBehaviour
         var queryGroup = from score in scores
                          group score by score % 2;
 
-        foreach(var group in queryGroup)
+        foreach (var group in queryGroup)
         {
             LogSystem.LogWithColor(group, "#f11");
 
-            foreach(var item in group)
+            foreach (var item in group)
             {
                 LogSystem.LogWithColor(item, "#79f");
             }
 
             LogSystem.LogWithColor("----- 群組分隔線 -----", "#79f");
+        } 
+        #endregion
+
+        // let 中間變數，可以取得特定的資料並且儲存於變數內
+         var queryRed = from item in items
+                        let firstWord = item[0]  // 先取得所有資料的第一個字
+                        where firstWord == '紅'
+                        select item;
+        foreach (var item in queryRed)
+        {
+            LogSystem.LogWithColor($"第一個字是紅的道具：{item}", "#fa3");
         }
+
+        // into 將資料分組，在獲得資料時可透過 Key 獲得分組名稱
+        var queryRedGroup = from item in items
+                            group item by item[0] into newGroup
+                            select newGroup;
+
+        foreach (var group in queryRedGroup)
+        {
+            LogSystem.LogWithColor($"群組：{group.Key}", "#3f3");
+            foreach (var item in group)
+            {
+                LogSystem.LogWithColor($"道具：{item}", "#9f9");
+            }
+        }
+
+        var queryEquals = from score in scores
+                          join number in numbers     // 加入第二組資料
+                          on score equals number     // on 比較兩組資料 
+                          // select score;
+                          select number;             // 選兩組資料結果都相等
+
+        foreach (var item in queryEquals)
+        {
+            LogSystem.LogWithColor($"兩組相同的數字：{item}", "#f33");
+        }
+
+        LogSystem.LogWithColor($"最大值：{queryEquals.Max()}", "#7f7");
+        LogSystem.LogWithColor($"最小值：{queryEquals.Min()}", "#7f7");
+        LogSystem.LogWithColor($"平均值：{queryEquals.Average()}", "#7f7");
+        LogSystem.LogWithColor($"總和：{queryEquals.Sum()}", "#7f7");
     }
 }
