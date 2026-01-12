62750c3ac78341f61fa58167adcd46e90d18e29d
lucky86622 <lucky86622@gmail.com>
2026-01-12 14:00:53 +0800

    課程2

---
+++ Assets/Scripts/Tetris/GameData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Puzzle.Tetris
+{
+    public class GameData
+    {
+        public int boardWidth;
+        public int boardHeight;
+
+        /// <summary>
+        /// 建構式(初始化class用)
+        /// </summary>
+        public GameData()
+        {
+            boardWidth = 10;
+            boardHeight = 20;
+        }
+
+        /// <summary>
+        /// 建構式(可自訂初始值版本)
+        /// </summary>
+        /// <param name="Width">寬</param>
+        /// <param name="Height">高</param>
+        public GameData(int Width, int Height)
+        {
+            boardWidth = Width;
+            boardHeight = Height;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Tetris/TetrisBasic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine; // 使用 XXXXX 命名空間
+
+// 命名空間(程式資料夾的概念) 第一層名稱(.的)次一層名稱
+namespace Puzzle.Tetris
+{
+    // 公開權限 類別 名稱 (:繼承) Unity基礎類別
+    public class TetrisBasic : MonoBehaviour
+    {
+        /// <summary>
+        /// [靜態]data資料物件實體
+        /// </summary>
+        private static GameData _data;
+        /// <summary>
+        /// [靜態]公開存取使用的data物件(唯讀)
+        /// </summary>
+        public static GameData data
+        {
+            get
+            {
+                if( _data == null)
+                {
+                    // 如果(資料實體 不存在) 建立新的
+                    _data = new GameData();
+                }
+                return _data;
+            }
+        }
+        
+        private void Start()
+        {
+            Debug.Log(data.boardWidth);
+            Debug.Log(data.boardHeight);
+        }
+    }
+}
+
