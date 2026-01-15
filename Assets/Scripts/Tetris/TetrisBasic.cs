5358dae9f51e1f142e3265d5d5e58284b2fb64bd
lucky86622 <lucky86622@gmail.com>
2026-01-15 14:00:33 +0800

    Update TetrisBasic.cs

--- Assets/Scripts/Tetris/TetrisBasic.cs
+++ Assets/Scripts/Tetris/TetrisBasic.cs
@@ -26,6 +26,25 @@ namespace Puzzle.Tetris
                 return _data;
             }
         }
+        /// <summary>
+        /// 經由分數計算出來的遊戲等級
+        /// </summary>
+        private int _level
+        {
+            get
+            {
+                // 級距：1000
+                return _score / 1000;
+            }
+        }
+        /// <summary>
+        /// 遊戲進行成績
+        /// </summary>
+        private int _score;
+        /// <summary>
+        /// 遊戲是否結束
+        /// </summary>
+        private bool _isGameOver;
         #endregion
 
         #region 遊戲核心資料結構
@@ -35,14 +54,14 @@ namespace Puzzle.Tetris
         public Brick brickTMP;
 
         /// <summary>
-        /// 
+        /// 棋盤載體UI
         /// </summary>
         public Transform boardUI;
 
         /// <summary>
         /// 遊戲棋盤二維陣列(複數集合物件)
         /// </summary>
-        private bool[,] _gameBorad;
+        private Brick[,] _gameBorad;
 
         /// <summary>
         /// 方塊種類(形狀)的列舉
@@ -55,19 +74,30 @@ namespace Puzzle.Tetris
 
         private void Start()
         {
+            InitialGame();
+        }
+
+        /// <summary>
+        /// 初始化遊戲
+        /// </summary>
+        private void InitialGame()
+        {
+            _score = 0;
+            _isGameOver = false;
+            _gameBorad = new Brick[data.boardWidth, data.boardHeight];
+
             // For迴圈：起始值；終點值；迭代值；
             for (int y = 0; y < data.boardHeight; y++)
             {
                 // 巢狀迴圈： 10 * 20 次
                 for (int x = 0; x < data.boardWidth; x++)
                 {
-                    // 具現化物件到特定目標
-                    Instantiate(brickTMP, boardUI).name = $"Brick({x}, {y})";
+                    // 棋盤[指定的座標] = 具現化物件到特定座標
+                    _gameBorad[x, y] = Instantiate(brickTMP, boardUI);
+                    // 為了辨識容易將每個Brick依座標命名
+                    _gameBorad[x, y].name = $"Brick({x}, {y})";
                 }
             }
-
-            _gameBorad = new bool[data.boardWidth, data.boardHeight];
-            Debug.Log(_gameBorad[0, 0]);
         }
     }
 }
